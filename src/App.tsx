51ed86a6120336d1202df7f6f6b0f74a760ae786
[dyad] <cletos27@gmail.com>
2026-01-20 01:44:07 +0000

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -59,7 +59,7 @@ const AppContent = () => {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
         <AuthProvider>
           <AppContent />
         </AuthProvider>
