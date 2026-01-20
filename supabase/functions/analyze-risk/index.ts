b6dd2f082c8be8f6d3c9778c03f42c57c159e102
[dyad] <cletos27@gmail.com>
2026-01-20 00:55:39 +0000

    Updated supabase/functions/analyze-risk/index.ts

--- supabase/functions/analyze-risk/index.ts
+++ supabase/functions/analyze-risk/index.ts
@@ -1,13 +1,8 @@
 // @ts-ignore
-
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
-
 // @ts-ignore
-
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.57.2";
-
 // @ts-ignore
-
 import { getCorsHeaders, handleOptions } from "../_shared/cors.ts";
 
 
